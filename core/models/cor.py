1ca931b942db0519a5f37c278538ed4fd369f9f7
= <=>
2024-06-28 09:14:51 -0300

    Cor

--- core/models/cor.py
+++ core/models/cor.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-class Acessorio(models.Model):
+class Cor(models.Model):
     descricao = models.CharField(max_length=100)
     
     def __str__(self):
         return f"{self.descricao} ({self.id})"
-    
-
-        
